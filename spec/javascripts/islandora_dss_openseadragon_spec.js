de45a030547236f4dd4427702ebfd098e468f9ee
jrgriffiniii <griffinj@lafayette.edu>
2013-12-12 18:00:54 -0500

    Initializing the repository

---
+++ spec/javascripts/islandora_dss_openseadragon_spec.js
@@ -0,0 +1,31 @@
+/**
+ * @file The Jasmine spec for the DSS OpenSeadragon widget
+ *
+ */
+
+describe("DSS Islandora OpenSeadragon", function() {
+
+	var seadragon;
+	var drupal;
+
+	beforeEach(function() {
+
+		// Drupal mock Object
+		drupal = { settings:
+			   { basePath: 'http://localhost/',
+			     islandoraOpenSeadragon:
+			     { resourceUri: 'http://uri/',
+			       pid: 'islandora:testObject',
+			       settings: { id: 'testId'}
+			     }
+			   }
+		};
+
+		seadragon = new DssOpenSeadragon($, drupal);
+	    });
+
+	it("should instantiate with a Drupal Object", function() {
+
+		expect(seadragon.$).toBe($);
+	    });
+    });
